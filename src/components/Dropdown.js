1b3d47dc754585701235d7bca08634d58782fd34
Kritsatorn Saengweang <kritsatornsaengweang@MacBook-Pro-4.local>
2020-11-21 03:18:13 +0700

    proj

---
+++ src/components/Dropdown.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import './Dropdown.css';
+import './Box.css';
+
+const Dropdown = ({prices}) => {
+    const [dropdown, setDropdown] = useState('');
+    return (
+        <form >
+            <select className="dropbox" value={dropdown} onChange={(e) => setDropdown(e.target.value)}>
+                {prices.map((price) => {
+                    return(
+                        <option>{price}</option>
+                    );
+                })}
+            </select>
+        </form>
+    );
+}
+
+export default Dropdown;
